2b7e888004cccf116338e419dc51575e05be8c69
Mikkel Stårvik <34859146+MStarvik@users.noreply.github.com>
2021-11-04 09:06:32 +0100

    Initialize test_action

---
+++ src/build/new_package/CMakeFiles/new_node.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/ROS2/src/new_package/src/new_node.cpp" "/workspaces/ROS2/src/build/new_package/CMakeFiles/new_node.dir/src/new_node.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/workspaces/ROS2/src/new_package/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/build/new_package/CMakeFiles/new_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/new_node.dir/src/new_node.cpp.o"
+  "new_node"
+  "new_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/new_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
